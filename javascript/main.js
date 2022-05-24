18a50f9723300575a132ec07c5b6aeea94d54601
Martinaf <martinaferguson97@gmail.com>
2022-05-24 11:47:03 -0300

    pruebas js

--- javascript/main.js
+++ javascript/main.js
@@ -1,2 +1,30 @@
+// alert('Hola! Bienvenido a Mondo Vino')
 
-alert('Hola! Bienvenido a Mondo Vino')
+// let edad
+// edad = prompt ('Ingrese su edad')
+//     if (edad <18){
+//         alert ('Eres menor de edad')//Si es menor de edad me gustaria que se quede en un loop y que no lo permita avanzar de ese punto.
+
+//     }else (edad > 18)
+//         alert ('holaaaaaaaaaaaaaaaaaaaa')
+
+function add(product, price){
+    console.log(product,price)
+
+}
+    
+
+
+    
+
+
+        
+
+
+
+
+
+
+
+
+
